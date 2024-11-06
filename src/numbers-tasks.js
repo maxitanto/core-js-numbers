c934a8663bf252e4e9ed73c2ec33bb3f680f15bb
Maksym Baldyniuk <frundzik1989@gmail.com>
2024-11-06 23:18:01 +0200

    feat: resolved to getAngleBetweenVectors

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -87,8 +87,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -(b / a);
 }
 
 /**
@@ -108,8 +108,12 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const mult = x1 * x2 + y1 * y2;
+  const sqrt1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const sqrt2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const cos = mult / (sqrt1 * sqrt2);
+  return Math.acos(cos);
 }
 
 /**
